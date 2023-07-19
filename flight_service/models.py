5658c210fcdae4bbc562489c8f70c7b57da08ba3
OleksandrYanchuk <sashajanchuck13@gmail.com>
2023-07-19 11:19:17 +0300

    add base model for Crew

--- flight_service/models.py
+++ flight_service/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255, unique=True)
+    last_name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Flight(models.Model):
+    pass
